38c603502b6144162c9092ed4077214b59108bb6
ilsteiner <izakizak@gmail.com>
2016-01-25 09:43:34 -0500

    Fixed JS to duplicate fields

--- static/js/libs/jquery.cloneform.js
+++ static/js/libs/jquery.cloneform.js
@@ -1,62 +1,68 @@
 //The jQuery Setup
-$(document).ready(function(){
-	
-	$('#insert-new').click(function(){
-		var yourclass=".relationship";  //The class you have used in your form
-		var clonecount = $(yourclass).length;	//how many clones do we already have?
-		var newid = Number(clonecount);		//Id of the new clone, we're zero indexed so don't add 1
-		
-		$(yourclass+":last").fieldclone({		//Clone the last element
-			newid_: newid,						//Id of the new clone, (you can pass your own if you want)
-			target_: $("#insert-new"),			//where do we insert the clone? (target element)
-			insert_: "after",					//where do we insert the clone? (after/before/append/prepend...)
-			limit_: 10							//Maximum Number of Clones
-		});
-		return false;
-	});
+$(document).ready(function () {
+
+    $('#insert-new').click(function () {
+        var yourclass = ".relationship";  //The class you have used in your form
+        var clonecount = $(yourclass).length;	//how many clones do we already have?
+        var newid = Number(clonecount);		//Id of the new clone, we're zero indexed so don't add 1
+
+        $(yourclass + ":last").fieldclone({		//Clone the last element
+            newid_: newid,						//Id of the new clone, (you can pass your own if you want)
+            target_: $("#insert-new"),			//where do we insert the clone? (target element)
+            insert_: "after",					//where do we insert the clone? (after/before/append/prepend...)
+            limit_: 10							//Maximum Number of Clones
+        });
+        return false;
+    });
 });
 
 //The Plugin Script
-(function($) {
+(function ($) {
 
-    $.fn.fieldclone = function(options) { 
-    
-		//==> Options <==//
-		var settings = {
-			newid_ : 0,
-			target_: $(this),
-			insert_: "after",
-			limit_: 0
-		};
-        if (options) $.extend(settings, options);           
+    $.fn.fieldclone = function (options) {
 
-		if( (settings.newid_ <= (settings.limit_+1)) || (settings.limit_==0) ){	//Check the limit to see if we can clone
+        //==> Options <==//
+        var settings = {
+            newid_: options.newid_,
+            target_: $(this),
+            insert_: options.insert_,
+            limit_: options.limit_
+        };
+        if (options) $.extend(settings, options);
 
-			//==> Clone <==//
-			var fieldclone = $(this).clone();
-			var node = $(this)[0].nodeName;
-			var classes = $(this).attr("class");
+        if ((settings.newid_ <= (settings.limit_ + 1)) || (settings.limit_ == 0)) {	//Check the limit to see if we can clone
 
-			//==> Increment every input id <==//
-			var srcid = 1;
-			$(fieldclone).find(':input').each(function(){
-				var s = $(this).attr("name"); 			
-				$(this).attr("name", s.replace(eval('/_'+srcid+'/ig'),'_'+settings.newid_)); 
-			});
+            //==> Clone <==//
+            var fieldclone = $(this).clone();
+            var node = $(this)[0].nodeName;
+            var classes = $(this).attr("class");
 
-			//==> Locate Target Id <==//
-			var targetid = $(settings.target_).attr("id");
-			if(targetid.length<=0){
-				targetid = "clonetarget";
-				$(settings.target_).attr("id",targetid);
-			}		
+            /*
+             * This is where the name and ID are incremented. I had to rewrite it for my use case.
+             * */
+            $(fieldclone).find(':input').each(function () {
+                oldname = $(this).attr("name");
+                oldid = $(this).attr("id");
+                $(this).attr("name", oldname.replace(/-\d-/, '-' + options.newid_ + '-'));
+                $(this).attr("id", oldname.replace(/-\d-/, '-' + options.newid_ + '-'));
+            });
 
-			//==> Insert Clone <==//
-			var newhtml = $(fieldclone).html().replace(/\n/gi,"");
-			newhtml = '<'+node+' class="'+classes+'">'+newhtml+'</'+node+'>';
-			
-			eval("var insertCall = $('#"+targetid+"')."+settings.insert_+"(newhtml)"); 
-		}
+            //Fix the legend for the fieldset
+            $(fieldclone).find('legend').html('New Relationship');
+
+            //==> Locate Target Id <==//
+            var targetid = $(settings.target_).attr("id");
+            if (targetid.length <= 0) {
+                targetid = "clonetarget";
+                $(settings.target_).attr("id", targetid);
+            }
+
+            //==> Insert Clone <==//
+            var newhtml = $(fieldclone).html().replace(/\n/gi, "");
+            newhtml = '<' + node + ' class="' + classes + '" id="' + 8 + '">' + newhtml + '</' + node + '>';
+
+            eval("var insertCall = $('#" + targetid + "')." + settings.insert_ + "(newhtml)");
+        }
     };
 
-})(jQuery);    
+})(jQuery);
