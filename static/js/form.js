15fd725f17bab84061a586c54a5b2891f3552c43
ilsteiner <izakizak@gmail.com>
2016-01-27 15:23:42 -0500

    Trying to get JS and CSS for hiding and showing the bidirectional form fields working

--- static/js/form.js
+++ static/js/form.js
@@ -2,7 +2,20 @@ $( document ).ready(function() {
     //Change related to label when selecting a new relationship
     $('.related_to').change(function() {
         $(this).closest('fieldset').find('legend').html('Relationship with ' + $(this).children('option:selected').text())
-    })
+    });
+
+    //When switching to a bidirectional relationship, show the rest of the form
+    $('input.bidirectional').checkboxpicker().change(function() {
+        console.log("HI!");
+        if($(this).prop('checked') === true){
+            $(this).parent().parent().child()
+            $(this).closest('div.bidirectional').removeClass('hidden');
+        }
+        else if($(this).prop('checked') === false){
+            $(this).closest('div.bidirectional').removeClass('hidden');
+        }
+
+    });
 
     //Activate checkbox JS
     $(':checkbox').checkboxpicker();
