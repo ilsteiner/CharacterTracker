4845bae5d13f8003c5b845cdf62a4898383575da
ilsteiner <izakizak@gmail.com>
2016-01-28 13:25:58 -0500

    Change labels when selecting relationship or changing name

--- static/js/form.js
+++ static/js/form.js
@@ -1,18 +1,74 @@
 $( document ).ready(function() {
-    //Change related to label when selecting a new relationship
-    $('.related_to').change(function() {
-        $(this).closest('fieldset').find('legend').html('Relationship with ' + $(this).children('option:selected').text());
-    });
-
     update_triggers();
 });
 
 function update_triggers() {
-    console.log('Updating triggers');
+    //Change related to label when selecting a new relationship
+    $('.related_to').change(function() {
+        /*var current_id = $(this).attr('id').match(/\d+/)[0];
+
+        if($('#name') === "" || $(this).children('option:selected').text() == 'Select an option'){
+            $('#sublegend-' + current_id).html("New Relationship");
+            $('#bidirectional-legend-' + current_id).text("Bidirectional Relationship");
+        }
+
+        else{
+            $('#sublegend-' + current_id).html(possesive_name($('#name').val())
+            + ' relationship with '
+            + $(this).children('option:selected').text());
+
+        $('#bidirectional-legend-' + current_id).text(possesive_name($(this).children('option:selected').text())
+            + " relationship with "
+            + $('#name').val());
+        }*/
+
+        update_legends($(this));
+    });
+
+    //Change related to label when changing name
+    $('#name').change(function () {
+        $('.sub-legend').each(function () {
+            var current_id = $(this).attr('id').match(/\d+/)[0];
+
+            var related_to_input = $('#relationships-' + current_id +'-related_to');
+
+            update_legends(related_to_input);
+        });
+    });
 
     //Toggle visibility of the bidirectional form
     $('input.bidirectional').change(function () {
         var the_id = $(this).attr('id').match(/\d+/)[0];
         $('#bidirectional-' + the_id).collapse('toggle');
     });
+}
+
+function possesive_name(name) {
+    name = $.trim(name)
+
+    if(name.match(/[sS]$/)) {
+        return name + "'";
+    }
+    else {
+        return name + "'s";
+    }
+}
+
+function update_legends(related_to_input) {
+    var current_id = related_to_input.attr('id').match(/\d+/)[0];
+
+    if($('#name').val() === "" || related_to_input.children('option:selected').text() == 'Select an option'){
+        $('#sublegend-' + current_id).html("New Relationship");
+        $('#bidirectional-legend-' + current_id).text("Bidirectional Relationship");
+    }
+
+    else{
+        $('#sublegend-' + current_id).html(possesive_name($('#name').val())
+        + ' relationship with '
+        + related_to_input.children('option:selected').text());
+
+    $('#bidirectional-legend-' + current_id).text(possesive_name(related_to_input.children('option:selected').text())
+        + " relationship with "
+        + $('#name').val());
+    }
 }
