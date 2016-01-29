9b455b8cd40781a3e9949e92808082a744275b25
ilsteiner <izakizak@gmail.com>
2016-01-29 10:08:08 -0500

    Wrapped names and added boundign box for graph

--- static/js/graph.js
+++ static/js/graph.js
@@ -9,23 +9,31 @@ function show_json() {
         $("<ul/>", {
             "class": "data-items",
             html: items.join("")
-        }).appendTo("#graph");
+        }).appendTo("#graph_container");
     });
 }
 
 //Largely copied from http://bl.ocks.org/jose187/4733747
 function show_graph() {
     var width = 960,
-        height = 500;
+        height = 500,
+        radius = 40;
 
-    var svg = d3.select("#graph").append("svg")
+    var svg = d3.select("#graph_container").append("svg")
         .attr("width", width)
-        .attr("height", height);
+        .attr("height", height)
+        .attr("id", "graph");
+
+    size_graph();
+
+    width = get_width();
+
+    height = get_height();
 
     var force = d3.layout.force()
         .gravity(.05)
-        .distance(250)
-        .charge(-75)
+        .distance(150)
+        .charge(-2000)
         .size([width, height]);
 
     d3.json('/graph-query.json', function (json) {
@@ -49,16 +57,35 @@ function show_graph() {
             .call(force.drag);
 
         node.append("circle")
-            .attr("r", "30")
+            .attr("r", radius);
 
         node.append("text")
-            .attr("dx", 12)
-            .attr("dy", ".35em")
-            .text(function (d) {
-                return d.name
+            .each(function (d) {
+                var name_array = d.name.split(" ");
+                for (i=0; i < name_array.length; i++){
+                    d3.select(this).append("tspan")
+                        .text(name_array[i])
+                        .attr("text-anchor", "middle")
+                        .attr("dy", i ? "1.2em" : 0)
+                        .attr("x", 0)
+                        .attr("class", "tspan-"+i);
+                }
             });
 
         force.on("tick", function () {
+            /*node.attr("dx", function (d) {
+                return d.x = Math.max(radius, Math.min(width - radius, d.x));
+            }).attr("dy", function (d) {
+               return d.y = Math.max(radius, Math.min(height - radius, d.y));
+            });
+*/
+
+            node.attr("transform", function (d) {
+                var dx = Math.max(radius, Math.min(width - radius, d.x));
+                var dy = Math.max(radius, Math.min(height - radius, d.y));
+                return "translate(" + dx + "," + dy + ")";
+            });
+
             link.attr("x1", function (d) {
                     return d.source.x;
                 })
@@ -72,21 +99,46 @@ function show_graph() {
                     return d.target.y;
                 });
 
-            node.attr("transform", function (d) {
+            /*node.attr("transform", function (d) {
                 return "translate(" + d.x + "," + d.y + ")";
-            });
+            });*/
         })
     });
 }
 
 function update_graph() {
     d3.json('/graph-query.json', function (data) {
-        d3.select("#graph")
+        d3.select("#graph_contianer")
             .append()
     });
 }
 
+function get_width() {
+    var graph = $('#graph');
+
+    var container = graph.parent();
+
+    return container.width();
+}
+
+function get_height() {
+    var graph = $('#graph');
+    var aspect = graph.width() / graph.height();
+    return Math.round(get_width() / aspect);
+}
+
+function size_graph(){
+    var graph = $('#graph');
+
+    graph.attr("width", get_width());
+
+    graph.attr("height", get_height());
+}
+
 $(function () {
     show_graph();
-    //show_json();
+
+    size_graph();
+
+    $(window).on("resize", size_graph());
 });
