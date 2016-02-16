63df191106e5933868db8d075a285a99d5ac58ff
ilsteiner <izakizak@gmail.com>
2016-02-16 10:43:22 -0500

    fix port for heroku config

--- app.py
+++ app.py
@@ -262,13 +262,6 @@ def populate_sample_data():
 # Launch.
 #----------------------------------------------------------------------------#
 
-# Default port:
-if __name__ == '__main__':
-    app.run()
-
-# Or specify port manually:
-'''
-if __name__ == '__main__':
+# Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-'''
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='CharacterTracker',
+        version='1',
+        packages=['_updated.app', '_updated.app.controllers', '_updated.tests', '_updated.config',
+                  '_updated.config.development'],
+        url='',
+        license='',
+        author='isteiner',
+        author_email='',
+        description=''
+)
